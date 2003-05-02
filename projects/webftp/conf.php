b9efba6d6c026fe29e5962bf73e780528194e448
Peter Postma <peter.postma@gmail.com>
2003-05-02 16:10:28 +0000

    That was a stupid typo :-)

--- projects/webftp/conf.php
+++ projects/webftp/conf.php
@@ -1,5 +1,5 @@
 <?
-  # $Id: conf.php,v 1.2 2003-05-02 16:06:54 peter Exp $
+  # $Id: conf.php,v 1.3 2003-05-02 16:10:28 peter Exp $
   #
   # WebFTP client - configuration
   #
@@ -13,7 +13,7 @@
   /* temporary file directory for downloads */
   $temp_dir = "/var/tmp";
 
-  /* timeout of the cookie in minutes */
+  /* timeout of the cookie in seconds */
   $cookie_timeout = 300;
 
 ?>
