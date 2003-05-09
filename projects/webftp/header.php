726b774789157aa881d34b826a9807bb095852c1
Peter Postma <peter.postma@gmail.com>
2003-05-09 17:10:06 +0000

    bump versions

--- projects/webftp/header.php
+++ projects/webftp/header.php
@@ -1,5 +1,5 @@
 <?
-  # $Id: header.php,v 1.1.1.1 2003-03-19 12:58:21 peter Exp $
+  # $Id: header.php,v 1.2 2003-05-09 17:10:06 peter Exp $
   #
   # WebFTP client - header
   #
@@ -8,7 +8,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-  <title>WebFTP v0.71-devel</title>
+  <title>WebFTP v0.72</title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body bgcolor="#E8E8E8">
