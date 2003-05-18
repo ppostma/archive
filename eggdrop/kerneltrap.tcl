ce08175ee05e6cc8e608154867f2be2600176138
Peter Postma <peter.postma@gmail.com>
2003-05-18 00:38:14 +0000

    add these two... i think that they work fine

---
+++ eggdrop/kerneltrap.tcl
@@ -0,0 +1,254 @@
+# $Id: kerneltrap.tcl,v 1.1 2003-05-18 00:38:14 peter Exp $
+
+# kerneltrap.tcl / KernelTrap.org News Announce Script for an eggdrop
+# version 1.0 / 18/05/2003 / by Peter Postma <peter@webdeveloping.nl>
+#
+# This script makes use of a function from alltools.tcl.
+# Please put alltools.tcl in your eggdrop configuration!
+#
+# This script also uses http.tcl. You *don't* need to put http.tcl
+# your eggdrop configuration!
+#
+# The kerneltrap.tcl script works best with TCL versions higher than 8.2.
+#
+# For Questions/suggestions/bug/etc: peter@webdeveloping.nl
+# If you found spelling/grammatical errors, please also mail me!
+#
+# Please change the configuration setting below:
+
+### Configuration settings ###
+
+# flags needed to use the trigger [default=everyone]
+set kerneltrap(flags) "-|-"
+
+# channels where the bot doesn't respond to triggers [seperate with spaces]
+set kerneltrap(nopub) "" 
+
+# the triggers: [seperate with spaces]
+set kerneltrap(triggers) "!kerneltrap"
+
+# method to send the messages:
+# 0 = Private message
+# 1 = Public message 
+# 2 = Private notice 
+# 3 = Public notice
+set kerneltrap(method) 1
+
+# display n headlines when a trigger is used [> 1]
+set kerneltrap(headlines) 2
+
+# below you can change the layout:
+# %title = titel of article
+# %link  = link to article
+# %b   = bold text
+# %u   = underlined text
+set kerneltrap(layout) "\[%bKernelTrap%b\] %title - %link"
+
+# announce the news automaticly in the channels? [0=no / 1=yes]
+set kerneltrap(autonews) 1
+
+# autonews: send to which channels? [seperate channels with spaces]
+set kerneltrap(autonewschan) "#channel1"
+
+# check for news after n minutes? [min. 30]
+# don't set this to low!
+set kerneltrap(updates) 60
+
+# max. amount of messages which will be displayed meanwhile automatic updates.
+# with this setting you can prevent channel flooding if the updates are high
+# (e.g. longer than a hour).
+set kerneltrap(automax) 3
+
+# trigger to turn off the autonews. [string]
+set kerneltrap(autontrigger) "!kerneltrapon"
+
+# trigger to turn on the autonews. [string]
+set kerneltrap(autofftrigger) "!kerneltrapoff"
+
+# flags needed to use the autonews[on/off] trigger [default=master]
+set kerneltrap(autotriggerflag) "m|m"
+
+# log additional information (debug) to the partyline? [0=no / 1=yes]
+set kerneltrap(log) 1
+
+### End Configuration settings ###
+
+
+
+### Begin TCL code ###
+
+set kerneltrap(version) "1.0"
+
+package require http
+
+for {set i 0} {$i < [llength $kerneltrap(triggers)]} {incr i} {
+  bind pub $kerneltrap(flags) [lindex $kerneltrap(triggers) $i] kerneltrap:pub
+  if {$kerneltrap(log)} { putlog "\[KernelTrap\] Trigger [lindex $kerneltrap(triggers) $i] added." }
+}
+unset i
+
+bind pub $kerneltrap(autotriggerflag) $kerneltrap(autofftrigger) kerneltrap:autoff
+bind pub $kerneltrap(autotriggerflag) $kerneltrap(autontrigger) kerneltrap:auton
+
+proc kerneltrap:getdata {} {
+  global kerneltrap kerneltrapdata
+
+  if {$kerneltrap(log)} { putlog "\[KernelTrap\] Updating data..." }
+  if {[info exists kerneltrapdata]} { unset kerneltrapdata }
+
+  set url "http://www.kerneltrap.org/module.php?mod=node&op=feed"
+  set page [::http::config -useragent "Mozilla"]
+
+  if {[catch {set page [::http::geturl $url -timeout 15000]} msg]} {
+    putlog "\[KernelTrap\] Problem: $msg"
+    return -1
+  }
+  
+  if {[::http::status $page] != "ok"} {
+    putlog "\[KernelTrap\] Problem: [::http::status $page]"
+    return -1
+  }
+
+  if {![regexp -nocase {ok} [::http::code $page]]} {
+    putlog "\[KernelTrap\] Problem: [::http::code $page]"
+    return -1
+  }
+
+  set lines [split [::http::data $page] \n]
+  set count 0
+  set item 0
+
+  for {set i 0} {$i < [llength $lines]} {incr i} {
+    set line [lindex $lines $i]
+    if {[regexp "<item>" $line trash]} { set item 1 }
+    if {[regexp "</item>" $line trash]} { set item 0 }
+    if {$item == 1} {
+      regexp "<title>(.*?)</title>" $line trash kerneltrapdata(title,$count)
+      if {[regexp "<link>(.*?)</link>" $line trash kerneltrapdata(link,$count)]} { incr count }
+    }
+  }
+  ::http::cleanup $page
+
+  unset url page msg count item lines
+  if {[info exists line]} { unset line }
+  if {[info exists trash]} { unset trash }
+
+  return 0
+}
+
+proc kerneltrap:pub {nick uhost hand chan text} {
+  global lastbind kerneltrap kerneltrapdata
+  if {[lsearch -exact $kerneltrap(nopub) [string tolower $chan]] >= 0} { return 0 }  
+
+  if {$kerneltrap(log)} { putlog "\[KernelTrap\] Trigger: $lastbind in $chan by $nick" }
+
+  if {[kerneltrap:getdata] != -1} {
+    for {set i 0} {$i < $kerneltrap(headlines)} {incr i} { kerneltrap:put $chan $nick $i $kerneltrap(method) }
+    unset i
+  } else {
+    putserv "NOTICE $nick :\[KernelTrap\] Something went wrong while updating."
+  }
+  if {[info exists kerneltrapdata]} { unset kerneltrapdata }
+}
+
+proc kerneltrap:put {chan nick which method} {
+  global kerneltrap kerneltrapdata
+
+  set outchan $kerneltrap(layout)
+  regsub -all "%title" $outchan $kerneltrapdata(title,$which) outchan
+  regsub -all "%link" $outchan $kerneltrapdata(link,$which) outchan
+  regsub -all "&amp;" $outchan "\\\&" outchan
+  regsub -all "%b"   $outchan "\002" outchan
+  regsub -all "%u"   $outchan "\037" outchan
+  switch -- $method {
+    0 { putserv "PRIVMSG $nick :$outchan" } 
+    1 { putserv "PRIVMSG $chan :$outchan" }
+    2 { putserv "NOTICE $nick :$outchan" }
+    3 { putserv "NOTICE $chan :$outchan" }
+    default { putserv "PRIVMSG $chan :$outchan" }
+  }
+  unset outchan
+}
+
+proc kerneltrap:update {} {
+  global kerneltrap kerneltrapdata kerneltrap_lastitem
+
+  if {[kerneltrap:getdata] != -1} {
+
+    if {![info exists kerneltrapdata(title,0)]} {
+      putlog "\[KernelTrap\] Something went wrong while updating."
+      return -1
+    }
+
+    if {![info exists kerneltrap_lastitem]} {
+      set kerneltrap_lastitem $kerneltrapdata(title,0)
+      if {$kerneltrap(log)} { putlog "\[KernelTrap\] Last news item set to $kerneltrapdata(title,0)" }
+    } else {
+      if {$kerneltrap(log)} { putlog "\[KernelTrap\] Last news item is $kerneltrapdata(title,0)" }
+    }
+
+    if {$kerneltrapdata(title,0) != $kerneltrap_lastitem} {
+      if {$kerneltrap(log)} { putlog "\[KernelTrap\] There's news!" }
+      for {set i 0} {$i < $kerneltrap(automax)} {incr i} {
+        if {$kerneltrapdata(title,$i) == $kerneltrap_lastitem} { break }
+        foreach chan [split $kerneltrap(autonewschan)] { kerneltrap:put $chan $chan $i 1 }
+        unset chan
+      }
+      unset i
+    } else {
+      if {$kerneltrap(log)} { putlog "\[KernelTrap\] No news." } 
+    }
+
+    set kerneltrap_lastitem $kerneltrapdata(title,0)
+  }
+
+  if {$kerneltrap(updates) < 30} {
+    timer 30 kerneltrap:update
+  } else {
+    timer $kerneltrap(updates) kerneltrap:update
+  }
+  if {[info exists kerneltrapdata]} { unset kerneltrapdata }
+
+  return 0
+}
+
+proc kerneltrap:autoff {nick uhost hand chan text} {
+  global lastbind kerneltrap kerneltrap_lastitem
+  if {[lsearch -exact $kerneltrap(nopub) [string tolower $chan]] >= 0} { return 0 }
+
+  if {$kerneltrap(log)} { putlog "\[KernelTrap\] Trigger: $lastbind in $chan by $nick" }
+
+  if {$kerneltrap(autonews) == 1} {
+    set kerneltrap(autonews) 0;  unset kerneltrap_lastitem
+    set whichtimer [timerexists "kerneltrap:update"]
+    if {$whichtimer != ""} { killtimer $whichtimer }
+    unset whichtimer
+    putlog "\[KernelTrap\] Autonews turned off."
+    putserv "PRIVMSG $chan :\001ACTION has turned the kerneltrap.org news announcer off.\001"
+  } else {
+    putserv "NOTICE $nick :My news announcer is already off!"
+  }
+}
+
+proc kerneltrap:auton {nick uhost hand chan text} {
+  global lastbind kerneltrap
+  if {[lsearch -exact $kerneltrap(nopub) [string tolower $chan]] >= 0} { return 0 }
+
+  if {$kerneltrap(log)} { putlog "\[KernelTrap\] Trigger: $lastbind in $chan by $nick" }
+
+  if {$kerneltrap(autonews) == 0} {
+    set kerneltrap(autonews) 1;  kerneltrap:update
+    putlog "\[KernelTrap\] Autonews turned on."
+    putserv "PRIVMSG $chan :\001ACTION has turned the kerneltrap.org news announcer on.\001"
+  } else {
+    putserv "NOTICE $nick :My news announcer is already on!"
+  }
+}
+
+set whichtimer [timerexists "kerneltrap:update"]
+if {$whichtimer != ""} { killtimer $whichtimer }
+unset whichtimer
+
+if {$kerneltrap(autonews) == 1} { kerneltrap:update }
+
+putlog "\[KernelTrap\] News Announcer version $kerneltrap(version): Loaded!"
---
+++ eggdrop/osnews.tcl
@@ -0,0 +1,254 @@
+# $Id: osnews.tcl,v 1.1 2003-05-18 00:38:14 peter Exp $
+
+# osnews.tcl / OSnews.org News Announce Script for an eggdrop
+# version 1.0 / 18/05/2003 / by Peter Postma <peter@webdeveloping.nl>
+#
+# This script makes use of a function from alltools.tcl.
+# Please put alltools.tcl in your eggdrop configuration!
+#
+# This script also uses http.tcl. You *don't* need to put http.tcl
+# your eggdrop configuration!
+#
+# The osnews.tcl script works best with TCL versions higher than 8.2.
+#
+# For Questions/suggestions/bug/etc: peter@webdeveloping.nl
+# If you found spelling/grammatical errors, please also mail me!
+#
+# Please change the configuration setting below:
+
+### Configuration settings ###
+
+# flags needed to use the trigger [default=everyone]
+set osnews(flags) "-|-"
+
+# channels where the bot doesn't respond to triggers [seperate with spaces]
+set osnews(nopub) "" 
+
+# the triggers: [seperate with spaces]
+set osnews(triggers) "!osnews"
+
+# method to send the messages:
+# 0 = Private message
+# 1 = Public message 
+# 2 = Private notice 
+# 3 = Public notice
+set osnews(method) 1
+
+# display n headlines when a trigger is used [> 1]
+set osnews(headlines) 2
+
+# below you can change the layout:
+# %title = titel of article
+# %link  = link to article
+# %b   = bold text
+# %u   = underlined text
+set osnews(layout) "\[%bOSnews%b\] %title - %link"
+
+# announce the news automaticly in the channels? [0=no / 1=yes]
+set osnews(autonews) 1
+
+# autonews: send to which channels? [seperate channels with spaces]
+set osnews(autonewschan) "#channel1"
+
+# check for news after n minutes? [min. 30]
+# don't set this to low!
+set osnews(updates) 60
+
+# max. amount of messages which will be displayed meanwhile automatic updates.
+# with this setting you can prevent channel flooding if the updates are high
+# (e.g. longer than a hour).
+set osnews(automax) 3
+
+# trigger to turn off the autonews. [string]
+set osnews(autontrigger) "!osnewson"
+
+# trigger to turn on the autonews. [string]
+set osnews(autofftrigger) "!osnewsoff"
+
+# flags needed to use the autonews[on/off] trigger [default=master]
+set osnews(autotriggerflag) "m|m"
+
+# log additional information (debug) to the partyline? [0=no / 1=yes]
+set osnews(log) 1
+
+### End Configuration settings ###
+
+
+
+### Begin TCL code ###
+
+set osnews(version) "1.0"
+
+package require http
+
+for {set i 0} {$i < [llength $osnews(triggers)]} {incr i} {
+  bind pub $osnews(flags) [lindex $osnews(triggers) $i] osnews:pub
+  if {$osnews(log)} { putlog "\[OSnews\] Trigger [lindex $osnews(triggers) $i] added." }
+}
+unset i
+
+bind pub $osnews(autotriggerflag) $osnews(autofftrigger) osnews:autoff
+bind pub $osnews(autotriggerflag) $osnews(autontrigger) osnews:auton
+
+proc osnews:getdata {} {
+  global osnews osnewsdata
+
+  if {$osnews(log)} { putlog "\[OSnews\] Updating data..." }
+  if {[info exists osnewsdata]} { unset osnewsdata }
+
+  set url "http://www.osnews.com/files/recent.rdf"
+  set page [::http::config -useragent "Mozilla"]
+
+  if {[catch {set page [::http::geturl $url -timeout 15000]} msg]} {
+    putlog "\[OSnews\] Problem: $msg"
+    return -1
+  }
+  
+  if {[::http::status $page] != "ok"} {
+    putlog "\[OSnews\] Problem: [::http::status $page]"
+    return -1
+  }
+
+  if {![regexp -nocase {ok} [::http::code $page]]} {
+    putlog "\[OSnews\] Problem: [::http::code $page]"
+    return -1
+  }
+
+  set lines [split [::http::data $page] \n]
+  set count 0
+  set item 0
+
+  for {set i 0} {$i < [llength $lines]} {incr i} {
+    set line [lindex $lines $i]
+    if {[regexp "<item>" $line trash]} { set item 1 }
+    if {[regexp "</item>" $line trash]} { set item 0 }
+    if {$item == 1} {
+      regexp "<title>(.*?)</title>" $line trash osnewsdata(title,$count)
+      if {[regexp "<link>(.*?)</link>" $line trash osnewsdata(link,$count)]} { incr count }
+    }
+  }
+  ::http::cleanup $page
+
+  unset url page msg count item lines
+  if {[info exists line]} { unset line }
+  if {[info exists trash]} { unset trash }
+
+  return 0
+}
+
+proc osnews:pub {nick uhost hand chan text} {
+  global lastbind osnews osnewsdata
+  if {[lsearch -exact $osnews(nopub) [string tolower $chan]] >= 0} { return 0 }  
+
+  if {$osnews(log)} { putlog "\[OSnews\] Trigger: $lastbind in $chan by $nick" }
+
+  if {[osnews:getdata] != -1} {
+    for {set i 0} {$i < $osnews(headlines)} {incr i} { osnews:put $chan $nick $i $osnews(method) }
+    unset i
+  } else {
+    putserv "NOTICE $nick :\[OSnews\] Something went wrong while updating."
+  }
+  if {[info exists osnewsdata]} { unset osnewsdata }
+}
+
+proc osnews:put {chan nick which method} {
+  global osnews osnewsdata
+
+  set outchan $osnews(layout)
+  regsub -all "%title" $outchan $osnewsdata(title,$which) outchan
+  regsub -all "%link" $outchan $osnewsdata(link,$which) outchan
+  regsub -all "&amp;" $outchan "\\\&" outchan
+  regsub -all "%b"   $outchan "\002" outchan
+  regsub -all "%u"   $outchan "\037" outchan
+  switch -- $method {
+    0 { putserv "PRIVMSG $nick :$outchan" } 
+    1 { putserv "PRIVMSG $chan :$outchan" }
+    2 { putserv "NOTICE $nick :$outchan" }
+    3 { putserv "NOTICE $chan :$outchan" }
+    default { putserv "PRIVMSG $chan :$outchan" }
+  }
+  unset outchan
+}
+
+proc osnews:update {} {
+  global osnews osnewsdata osnews_lastitem
+
+  if {[osnews:getdata] != -1} {
+
+    if {![info exists osnewsdata(title,0)]} {
+      putlog "\[OSnews\] Something went wrong while updating."
+      return -1
+    }
+
+    if {![info exists osnews_lastitem]} {
+      set osnews_lastitem $osnewsdata(title,0)
+      if {$osnews(log)} { putlog "\[OSnews\] Last news item set to $osnewsdata(title,0)" }
+    } else {
+      if {$osnews(log)} { putlog "\[OSnews\] Last news item is $osnewsdata(title,0)" }
+    }
+
+    if {$osnewsdata(title,0) != $osnews_lastitem} {
+      if {$osnews(log)} { putlog "\[OSnews\] There's news!" }
+      for {set i 0} {$i < $osnews(automax)} {incr i} {
+        if {$osnewsdata(title,$i) == $osnews_lastitem} { break }
+        foreach chan [split $osnews(autonewschan)] { osnews:put $chan $chan $i 1 }
+        unset chan
+      }
+      unset i
+    } else {
+      if {$osnews(log)} { putlog "\[OSnews\] No news." } 
+    }
+
+    set osnews_lastitem $osnewsdata(title,0)
+  }
+
+  if {$osnews(updates) < 30} { 
+    timer 30 osnews:update
+  } else {
+    timer $osnews(updates) osnews:update
+  }
+  if {[info exists osnewsdata]} { unset osnewsdata }
+
+  return 0
+}
+
+proc osnews:autoff {nick uhost hand chan text} {
+  global lastbind osnews osnews_lastitem
+  if {[lsearch -exact $osnews(nopub) [string tolower $chan]] >= 0} { return 0 }
+
+  if {$osnews(log)} { putlog "\[OSnews\] Trigger: $lastbind in $chan by $nick" }
+
+  if {$osnews(autonews) == 1} {
+    set osnews(autonews) 0;  unset osnews_lastitem
+    set whichtimer [timerexists "osnews:update"]
+    if {$whichtimer != ""} { killtimer $whichtimer }
+    unset whichtimer
+    putlog "\[OSnews\] Autonews turned off."
+    putserv "PRIVMSG $chan :\001ACTION has turned the osnews.org news announcer off.\001"
+  } else {
+    putserv "NOTICE $nick :My news announcer is already off!"
+  }
+}
+
+proc osnews:auton {nick uhost hand chan text} {
+  global lastbind osnews
+  if {[lsearch -exact $osnews(nopub) [string tolower $chan]] >= 0} { return 0 }
+
+  if {$osnews(log)} { putlog "\[OSnews\] Trigger: $lastbind in $chan by $nick" }
+
+  if {$osnews(autonews) == 0} {
+    set osnews(autonews) 1;  osnews:update
+    putlog "\[OSnews\] Autonews turned on."
+    putserv "PRIVMSG $chan :\001ACTION has turned the osnews.org news announcer on.\001"
+  } else {
+    putserv "NOTICE $nick :My news announcer is already on!"
+  }
+}
+
+set whichtimer [timerexists "osnews:update"]
+if {$whichtimer != ""} { killtimer $whichtimer }
+unset whichtimer
+
+if {$osnews(autonews) == 1} { osnews:update }
+
+putlog "\[OSnews\] News Announcer version $osnews(version): Loaded!"
