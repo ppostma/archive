cfd06842f12c7e7ec9b4093cfb6be34aaecf0143
Peter Postma <peter.postma@gmail.com>
2003-07-07 09:00:55 +0000

    added again, should work ok now

---
+++ eggdrop/phpdoc.tcl
@@ -0,0 +1,125 @@
+# $Id: phpdoc.tcl,v 1.3 2003-07-07 09:00:55 peter Exp $
+
+# PHP Doc for the eggdrop
+# version 0.3, 07/07/2003, by Peter Postma <peter@webdeveloping.nl>
+
+### Configuration settings ###
+
+# flags needed to use the triggers
+set phpdoc(flags) "-|-"
+
+# channels where the bot doesn't responds to triggers (seperate with spaces)
+set phpdoc(nopub) ""
+
+# the triggers: [seperate with spaces]
+set phpdoc(triggers) "!php !phpdoc"
+
+# flood protection: seconds between use of the triggers
+# to disable: set it to 0
+set phpdoc(antiflood) 5
+
+# method to send the messages:
+# 0 = Private message
+# 1 = Public message
+# 2 = Private notice
+# 3 = Public notice
+set phpdoc(method) 1
+
+# below you can change the layout of the output:
+# %syntax = function syntax
+# %descr  = description for the function
+# %func   = function name
+# %b      = bold text
+# %u      = underlined text
+set phpdoc(layout) "%syntax - %descr - http://nl.php.net/%func"
+
+# message layout when the function doesn't match:
+# %func   = function name
+# %b      = bold text
+# %u      = underlined text
+set phpdoc(nomatch) "No match for '%func' - try http://nl.php.net/%func ?"
+
+# phpdoc funcsummary.txt file
+set phpdoc(file) "/usr/home/peter/eggdrop/scripts/my/funcsummary.txt"
+
+### End Configuration settings ###
+
+
+
+### Begin TCL code ###
+
+set phpdoc(version) 0.3
+
+if {[info tclversion] < 8.1} {
+  putlog "Cannot load [file tail [info script]]: You need at least TCL version 8.1 and you have TCL version [info tclversion]."
+  return 1
+}
+
+for {set i 0} {$i < [llength $phpdoc(triggers)]} {incr i} {
+  bind pub $phpdoc(flags) [lindex $phpdoc(triggers) $i] pub:phpdoc
+}
+catch { unset i }
+
+proc pub:phpdoc {nick uhost hand chan text} {
+  global lastbind phpdoc
+
+  if {[lsearch -exact $phpdoc(nopub) [string tolower $chan]] >= 0} { return 0 }
+
+  if {[string length [string trim [lindex $text 0]]] == 0} {
+    putquick "NOTICE $nick :Syntax: $lastbind <function>"
+    return 0
+  }
+
+  if {[info exists phpdoc(floodprot)]} {
+    set diff [expr [clock seconds] - $phpdoc(floodprot)]
+    if {$diff < $phpdoc(antiflood)} {
+      putquick "NOTICE $nick :Trigger has just been used! Please wait [expr $phpdoc(antiflood) - $diff] seconds..."
+      return 0
+    }
+    catch { unset diff }
+  }
+  set phpdoc(floodprot) [clock seconds]
+
+  set input [string trim [lindex $text 0]]
+
+  switch -- $phpdoc(method) {
+    0 { set method "PRIVMSG $nick" }
+    1 { set method "PRIVMSG $chan" }
+    2 { set method "NOTICE $nick" }
+    3 { set method "NOTICE $chan" }
+    default { set method "PRIVMSG $chan" }
+  }
+
+  if {[catch { set fd [open $phpdoc(file) r] } err]} {
+    putquick "$method :$err"
+    return 0
+  }
+
+  while {[gets $fd line] >= 0} {
+    if {[regexp "^(.+) $input\\((.*?)\\)" $line]} {
+      gets $fd next
+
+      set outchan $phpdoc(layout)
+      regsub -all "%syntax" $outchan [string trim $line] outchan
+      regsub -all "%descr" $outchan [string trim $next] outchan
+      regsub -all "%func" $outchan $input outchan
+      regsub -all "%b" $outchan "\002" outchan
+      regsub -all "%u" $outchan "\037" outchan
+      putquick "$method :$outchan"
+
+      close $fd
+      return 0
+    }
+  }
+
+  set outchan $phpdoc(nomatch)
+  regsub -all "%func" $outchan $input outchan
+  regsub -all "%b" $outchan "\002" outchan
+  regsub -all "%u" $outchan "\037" outchan
+  putquick "$method :$outchan"
+
+  close $fd
+  return 0
+}
+
+putlog "PHPdoc version $phpdoc(version): loaded!"
