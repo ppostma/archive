12e56dcc0b69a85751f2222d2b1f72c14314ddc5
Peter Postma <peter.postma@gmail.com>
2003-08-01 21:34:26 +0000

    change /var/tmp to /tmp, it's more standard.

--- projects/webftp/conf.php
+++ projects/webftp/conf.php
@@ -1,5 +1,5 @@
 <?
-  # $Id: conf.php,v 1.3 2003-05-02 16:10:28 peter Exp $
+  # $Id: conf.php,v 1.4 2003-08-01 21:34:26 peter Exp $
   #
   # WebFTP client - configuration
   #
@@ -11,7 +11,7 @@
   $change = false;
 
   /* temporary file directory for downloads */
-  $temp_dir = "/var/tmp";
+  $temp_dir = "/tmp";
 
   /* timeout of the cookie in seconds */
   $cookie_timeout = 300;
