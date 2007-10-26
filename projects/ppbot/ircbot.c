4115776a7c2ddb0aeb5bea406fd539ce7b66e4b2
Peter Postma <peter.postma@gmail.com>
2007-10-26 13:29:13 +0000

    Print messages when starting and shutting down.  Print messages when receiving SIGINT/SIGTERM.

--- projects/ppbot/ircbot.c
+++ projects/ppbot/ircbot.c
@@ -27,6 +27,7 @@
 #include <sys/types.h>
 
 #include <errno.h>
+#include <fcntl.h>
 #include <poll.h>
 #include <signal.h>
 #include <stdio.h>
@@ -41,16 +42,21 @@
 /*
  * Local function prototypes.
  */
-static void usage(const char *);
-static void signal_handler(int);
-static void reconfigure(const char *);
-static void main_loop(const char *);
+static void ircbot_print_usage(const char *);
+static void ircbot_print_welcome(void);
+static void ircbot_print_daemon(pid_t);
+static void ircbot_print_bye(void);
+
+static void ircbot_signal_handler(int);
+static void ircbot_reconfigure(const char *);
+static void ircbot_main_loop(const char *);
 
 /*
- * Signal indicators.  quit - INT/TERM, reconfig - HUP.
+ * Signal indicators.
  */
-static volatile sig_atomic_t quit = FALSE;
-static volatile sig_atomic_t reconfig = FALSE;
+static volatile sig_atomic_t sig_int = FALSE;
+static volatile sig_atomic_t sig_term = FALSE;
+static volatile sig_atomic_t sig_hup = FALSE;
 
 /*
  * main --
@@ -61,12 +67,17 @@ main(int argc, char *argv[])
 {
 	const char *cfg = IRCBOT_CONFIG;
 	char	   *progname;
-	int	    ch, debug = FALSE;
+	int	    ch, fd, debug = FALSE;
+	pid_t	    child;
 
 	if ((progname = strrchr(argv[0], '/')) != NULL)
 		progname++;
 	else
 		progname = argv[0];
+
+	/* Enable logging to stderr if started from a terminal. */
+	if (isatty(STDERR_FILENO))
+		set_logstderr(TRUE);
 
 	/* Parse command line arguments. */
 	while ((ch = getopt(argc, argv, "df:")) != -1) {
@@ -78,19 +89,22 @@ main(int argc, char *argv[])
 			cfg = optarg;
 			break;
 		default:
-			usage(progname);
+			ircbot_print_usage(progname);
 		}
 	}
 
-	signal(SIGHUP, signal_handler);
-	signal(SIGINT, signal_handler);
-	signal(SIGTERM, signal_handler);
+	signal(SIGHUP, ircbot_signal_handler);
+	signal(SIGINT, ircbot_signal_handler);
+	signal(SIGTERM, ircbot_signal_handler);
 
 	set_logdebug(debug);
-	set_logstderr(TRUE);
 
-	if (config_parse(cfg) == FALSE)
+	ircbot_print_welcome();
+
+	if (config_parse(cfg) == FALSE) {
+		ircbot_print_bye();
 		exit(EXIT_FAILURE);
+	}
 
 	connections_verify();
 
@@ -99,15 +113,40 @@ main(int argc, char *argv[])
 
 	connections_initialize();
 
+	/* Become a daemon when flag -d is not present. */
 	if (!debug) {
-		if (daemon(1, 0) == -1) {
-			log_warn("Unable to daemonize");
+		switch (child = fork()) {
+		case -1:
+			log_warnx("Unable to fork process");
+			exit(EXIT_FAILURE);
+		case 0:
+			break;
+		default:
+			ircbot_print_daemon(child);
+			_exit(0);
+		}
+
+		if (setsid() == -1) {
+			log_warn("Unable to create new session");
 			exit(EXIT_FAILURE);
 		}
+
+		fd = open("/dev/null", O_RDWR);
+		if (fd != -1) {
+			dup2(fd, STDIN_FILENO);
+			dup2(fd, STDOUT_FILENO);
+			dup2(fd, STDERR_FILENO);
+
+			if (fd > STDERR_FILENO)
+				close(fd);
+		}
+
 		set_logstderr(FALSE);
 	}
 
-	main_loop(cfg);
+	ircbot_main_loop(cfg);
+
+	ircbot_print_bye();
 
 	connections_close();
 	connections_destroy();
@@ -121,42 +160,96 @@ main(int argc, char *argv[])
 }
 
 /*
- * usage --
+ * ircbot_print_usage --
  *	Print a message how to use the program and exit.
  */
 static void
-usage(const char *progname)
+ircbot_print_usage(const char *progname)
 {
 	fprintf(stderr, "Usage: %s [-d] [-f configfile]\n", progname);
 	exit(EXIT_FAILURE);
 }
 
 /*
- * signal_handler --
+ * ircbot_print_welcome --
+ *	Print a welcome message.
+ */
+static void
+ircbot_print_welcome(void)
+{
+	time_t	 t = time(NULL);
+	char	*date;
+
+	date = ctime(&t);
+	date[strlen(date) - 1] = '\0';
+
+	log_warnx("%s version %s started at %s", IRCBOT_NAME,
+	    IRCBOT_VERSION, date);
+}
+
+/*
+ * ircbot_print_daemon --
+ *	Print a running as daemon message.
+ */
+static void
+ircbot_print_daemon(pid_t child)
+{
+	time_t	 t = time(NULL);
+	char	*date;
+
+	date = ctime(&t);
+	date[strlen(date) - 1] = '\0';
+
+	log_warnx("%s version %s daemonized at %s, pid %u",
+	    IRCBOT_NAME, IRCBOT_VERSION, date, (unsigned int)child);
+}
+
+/*
+ * ircbot_print_bye --
+ *	Print a farewell message.
+ */
+static void
+ircbot_print_bye(void)
+{
+	time_t	 t = time(NULL);
+	char	*date;
+
+	date = ctime(&t);
+	date[strlen(date) - 1] = '\0';
+
+	log_warnx("%s version %s shutting down at %s", IRCBOT_NAME,
+	    IRCBOT_VERSION, date);
+}
+
+/*
+ * ircbot_signal_handler --
  *	Catch signals and set the signal indicators.
  */
 static void
-signal_handler(int sig)
+ircbot_signal_handler(int sig)
 {
 	switch (sig) {
 	case SIGTERM:
+		sig_term = TRUE;
+		break;
 	case SIGINT:
-		quit = TRUE;
+		sig_int = TRUE;
 		break;
 	case SIGHUP:
-		reconfig = TRUE;
+		sig_hup = TRUE;
 		break;
 	}
 }
 
 /*
- * reconfigure --
- *	Re-read the configuration file.  Can load new plugins, start new
- *	connections, join new channels, etc...
- *	This function is executed when receiving the HUP signal.
+ * ircbot_reconfigure --
+ *	Used to reconfigure the ircbot when already active.
+ *
+ *	Reads the configuration file, reloads plugins, starts new connections
+ *	and joins new channels.
  */
 static void
-reconfigure(const char *cfg)
+ircbot_reconfigure(const char *cfg)
 {
 	int rv;
 
@@ -182,12 +275,12 @@ reconfigure(const char *cfg)
 }
 
 /*
- * main_loop --
+ * ircbot_main_loop --
  *	The program main loop: wait with poll() for data from the IRC
  *	server(s), read/parse the data and handle the timers.
  */
 static void
-main_loop(const char *cfg)
+ircbot_main_loop(const char *cfg)
 {
 	struct connection *conn;
 	struct pollfd	  *pfd;
@@ -197,15 +290,25 @@ main_loop(const char *cfg)
 
 	gettimeofday(&base, NULL);
 
-	while (quit == FALSE) {
+	for (;;) {
 		/* Run expired timers. */
 		timer_run_expired();
 
 		/* Reload the configuration if we have been asked to. */
-		if (reconfig) {
-			reconfig = FALSE;
-			reconfigure(cfg);
+		if (sig_hup) {
+			sig_hup = FALSE;
+			ircbot_reconfigure(cfg);
 			log_warnx("The configuration has been reloaded.");
+		}
+
+		/* Shut down when receiving INT/TERM signal. */
+		if (sig_int) {
+			log_warnx("Received SIGINT, shutting down.");
+			break;
+		}
+		if (sig_term) {
+			log_warnx("Received SIGTERM, shutting down.");
+			break;
 		}
 
 		/* Correct the timers when running backwards. */
